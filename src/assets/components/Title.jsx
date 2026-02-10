bbed315bab2cee0632a4d0e05582dc9c92d32ecb
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-10 14:06:29 +0100

    added function

--- src/assets/components/Title.jsx
+++ src/assets/components/Title.jsx
@@ -1,5 +1,19 @@
+// IMPORTS
+import { useState } from "react";
+
 export default function Title() {
+  // LIST OF ITEMS
   const titles = ["Articolo 1", "Articolo 2 ", "Articolo 3 "];
+
+  //   useSTATE
+  const [currentIndex, setCurrentIndex] = useState("");
+
+  //   function for form
+  const inputFinder = (e) => setCurrentIndex(e.target.value);
+  const inputSubmit = (e) => {
+    e.preventDefault();
+    titles.push(currentIndex);
+  };
   return (
     <div className="container-sm">
       <div className="row">
@@ -8,6 +22,24 @@ export default function Title() {
             {element}
           </h2>
         ))}
+
+        <form onSubmit={inputSubmit}>
+          <div className="input-group mb-3 mt-5">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Inserire Input"
+              aria-label="Inserire Input"
+              aria-describedby="button-addon2"
+              //   button Commands
+              onChange={inputFinder}
+              value={currentIndex}
+            />
+            <button className="btn btn-outline-secondary" id="button-addon2">
+              Aggiungi
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
