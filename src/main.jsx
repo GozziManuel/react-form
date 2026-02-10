cb02248f0565abf8a6f2ec9b87c700a8ddfd8d7a
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-10 13:01:04 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/css/index.css";
+// css bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+// my CSS
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
