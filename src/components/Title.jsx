4dc5e14b9222cf5a8292ae82c6699b2a67dd08c9
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-11 17:20:53 +0100

    order fixed console problems

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ const titles = ["Articolo 1", "Articolo 2 ", "Articolo 3 "];
 
 export default function Title() {
   //   useSTATE
-  const [setTitle, setCurrentTitle] = useState(titles);
+  const [title, SetTitle] = useState(titles);
   const [currentIndex, setCurrentIndex] = useState("");
 
   // Toggling Change button
@@ -13,36 +13,32 @@ export default function Title() {
 
   // State For changing Title
   const [editTitleIndex, setEditTitleIndex] = useState(0);
-  const editedTitle = setTitle[editTitleIndex];
+  const editedTitle = title[editTitleIndex];
 
   // Function to declare wich Article
   const changingTitle = (EditIndex) => {
     setEditTitleIndex(EditIndex);
-    setEditTitle(setTitle[EditIndex]);
+    setEditTitle(title[EditIndex]);
   };
 
   //   function for form
   const inputFinder = (e) => setCurrentIndex(e.target.value);
   const inputSubmit = (e) => {
     e.preventDefault();
-    setCurrentTitle([...setTitle, currentIndex]);
+    SetTitle([...title, currentIndex]);
   };
   //   function for buttons
   const deleteOptionButton = (deleteButton) => {
-    setCurrentTitle(
-      setTitle.filter((element, index) => index !== deleteButton),
-    );
+    SetTitle(title.filter((element, index) => index !== deleteButton));
   };
 
   return (
     <div className="container-sm">
       <div>
         <div className="row">
-          {setTitle.map((element, index) => (
-            <div className="col-4 d-flex">
-              <h2 className="text-center m-5" key={index}>
-                {element}
-              </h2>
+          {title.map((element, index) => (
+            <div className="col-4 d-flex " key={index}>
+              <h2 className="text-center m-5">{element}</h2>
               <div className="btncontainer">
                 <button
                   className="btn btn-danger"
@@ -83,16 +79,17 @@ export default function Title() {
           </div>
         </form>
 
-        <form class="input-group mb-3">
+        <form className="input-group mb-3">
           <h1>{editedTitle}</h1>
           <input
             value={editTitle}
+            onChange={""}
             type="text"
             className="form-control"
             placeholder="Change Title"
             aria-describedby="button-addon2"
           />
-          <button class="btn btn-primary">Change</button>
+          <button className="btn btn-primary">Change</button>
         </form>
       </div>
     </div>
